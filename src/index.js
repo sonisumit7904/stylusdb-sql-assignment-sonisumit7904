4efbbd70c1461f342b9a4425ba98daea9c4a7bb2
Sumit Soni <122284205+sonisumit7904@users.noreply.github.com>
2024-04-04 15:22:16 +0530

    pass test9

--- src/index.js
+++ src/index.js
@@ -1,219 +1,8 @@
-// src/index.js
-
-// const parseQuery = require("./queryParser");
-// const readCSV = require("./csvReader");
-
-// async function executeSELECTQuery(query) {
-//   // const { fields, table, whereClauses } = parseQuery(query);
-//   // const data = await readCSV(`${table}.csv`);
-
-//   // src/index.js at executeSELECTQuery
-
-//   // Now we will have joinTable, joinCondition in the parsed query
-//   const { fields, table, whereClauses, joinTable, joinCondition } =
-//     parseQuery(query);
-//   data = await readCSV(`${table}.csv`);
-
-//   // Perform INNER JOIN if specified
-//   if (joinTable && joinCondition) {
-//     const joinData = await readCSV(`${joinTable}.csv`);
-//     data = data.flatMap((mainRow) => {
-//       return joinData
-//         .filter((joinRow) => {
-//           const mainValue = mainRow[joinCondition.left.split(".")[1]];
-//           const joinValue = joinRow[joinCondition.right.split(".")[1]];
-//           return mainValue === joinValue;
-//         })
-//         .map((joinRow) => {
-//           return fields.reduce((acc, field) => {
-//             const [tableName, fieldName] = field.split(".");
-//             acc[field] =
-//               tableName === table ? mainRow[fieldName] : joinRow[fieldName];
-//             return acc;
-//           }, {});
-//         });
-//     });
-//   }
-
-//   //7TH STEP
-//   // Apply WHERE clause filtering
-//   // const filteredData = whereClauses.length > 0
-//   //     ? data.filter(row => whereClauses.every(clause => {
-//   //         // You can expand this to handle different operators
-//   //         return row[clause.field] === clause.value;
-//   //     }))
-//   //     : data;
-//   //
-
-//   const filteredData =
-//     whereClauses.length > 0
-//       ? data.filter((row) =>
-//           whereClauses.every((clause) => evaluateCondition(row, clause))
-//         )
-//       : data;
-
-//   // Select the specified fields
-//   return filteredData.map((row) => {
-//     const selectedRow = {};
-//     fields.forEach((field) => {
-//       selectedRow[field] = row[field];
-//     });
-//     return selectedRow;
-//   });
-// }
-// async function executeSELECTQuery(query) {
-//     const { fields, table } = parseQuery(query);
-//     const data = await readCSV(`${table}.csv`);
-    
-//     // Filter the fields based on the query
-//     return data.map(row => {
-//         const filteredRow = {};
-//         fields.forEach(field => {
-//             filteredRow[field] = row[field];
-//         });
-//         return filteredRow;
-//     });
-// }
-
-//step5
-// src/index.js
-
-// const parseQuery = require('./queryParser');
-// const readCSV = require('./csvReader');
-
-// async function executeSELECTQuery(query) {
-//     const { fields, table, whereClause } = parseQuery(query);
-//     const data = await readCSV(`${table}.csv`);
-    
-//     // Filtering based on WHERE clause
-//     const filteredData = whereClause
-//         ? data.filter(row => {
-//             const [field, value] = whereClause.split('=').map(s => s.trim());
-//             return row[field] === value;
-//         })
-//         : data;
-
-//     // Selecting the specified fields
-//     return filteredData.map(row => {
-//         const selectedRow = {};
-//         fields.forEach(field => {
-//             selectedRow[field] = row[field];
-//         });
-//         return selectedRow;
-//     });
-// }
-
-// module.exports = executeSELECTQuery;
-
-
-
-// // src/index.js
-// function evaluateCondition(row, clause) {
-//   const { field, operator, value } = clause;
-//   switch (operator) {
-//     case "=":
-//       return row[field] === value;
-//     case "!=":
-//       return row[field] !== value;
-//     case ">":
-//       return row[field] > value;
-//     case "<":
-//       return row[field] < value;
-//     case ">=":
-//       return row[field] >= value;
-//     case "<=":
-//       return row[field] <= value;
-//     default:
-//       throw new Error(`Unsupported operator: ${operator}`);
-//   }
-// }
-
-// // module.exports = executeSELECTQuery;
-
-//step6
-// src/index.js
-
-// const parseQuery = require('./queryParser');
-// const readCSV = require('./csvReader');
-
-// async function executeSELECTQuery(query) {
-//     const { fields, table, whereClauses } = parseQuery(query);
-//     const data = await readCSV(`${table}.csv`);
-
-//     // Apply WHERE clause filtering
-//     const filteredData = whereClauses.length > 0
-//         ? data.filter(row => whereClauses.every(clause => {
-//             // You can expand this to handle different operators
-//             return row[clause.field] === clause.value;
-//         }))
-//         : data;
-
-//     // Select the specified fields
-//     return filteredData.map(row => {
-//         const selectedRow = {};
-//         fields.forEach(field => {
-//             selectedRow[field] = row[field];
-//         });
-//         return selectedRow;
-//     });
-// }
-
-// module.exports = executeSELECTQuery;
-
-//step7
-// src/index.js
-
-// const parseQuery = require('./queryParser');
-// const readCSV = require('./csvReader');
-
-// async function executeSELECTQuery(query) {
-//     const { fields, table, whereClauses } = parseQuery(query);
-//     const data = await readCSV(`${table}.csv`);
-
-//     // Apply WHERE clause filtering
-//     const filteredData = whereClauses.length > 0
-//     ? data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
-//     : data;
-
-//     // Select the specified fields
-//     return filteredData.map(row => {
-//         const selectedRow = {};
-//         fields.forEach(field => {
-//             selectedRow[field] = row[field];
-//         });
-//         return selectedRow;
-//     });
-// }
-
-// function evaluateCondition(row, clause) {
-//     const { field, operator, value } = clause;
-//     switch (operator) {
-//         case '=': return row[field] === value;
-//         case '!=': return row[field] !== value;
-//         case '>': return row[field] > value;
-//         case '<': return row[field] < value;
-//         case '>=': return row[field] >= value;
-//         case '<=': return row[field] <= value;
-//         default: throw new Error(`Unsupported operator: ${operator}`);
-//     }
-// }
-// module.exports = executeSELECTQuery;
-
-//step8
-const parseQuery = require('./queryParser');
+const { parseQuery } = require('./queryParser');
 const readCSV = require('./csvReader');
 
-async function executeSELECTQuery(query) {
-    // src/index.js at executeSELECTQuery
-
-// Now we will have joinTable, joinCondition in the parsed query
-const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
-let data = await readCSV(`${table}.csv`);
-
-// Perform INNER JOIN if specified
-if (joinTable && joinCondition) {
-    const joinData = await readCSV(`${joinTable}.csv`);
-    data = data.flatMap(mainRow => {
+function performInnerJoin(data, joinData, joinCondition, fields, table) {
+    return data.flatMap(mainRow => {
         return joinData
             .filter(joinRow => {
                 const mainValue = mainRow[joinCondition.left.split('.')[1]];
@@ -230,10 +19,99 @@ if (joinTable && joinCondition) {
     });
 }
 
-// Apply WHERE clause filtering after JOIN (or on the original data if no join)
-const filteredData = whereClauses.length > 0
-    ? data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
-    : data;
+function performLeftJoin(data, joinData, joinCondition, fields, table) {
+    return data.flatMap(mainRow => {
+        const matchingJoinRows = joinData.filter(joinRow => {
+            const mainValue = getValueFromRow(mainRow, joinCondition.left);
+            const joinValue = getValueFromRow(joinRow, joinCondition.right);
+            return mainValue === joinValue;
+        });
+
+        if (matchingJoinRows.length === 0) {
+            return [createResultRow(mainRow, null, fields, table, true)];
+        }
+
+        return matchingJoinRows.map(joinRow => createResultRow(mainRow, joinRow, fields, table, true));
+    });
+}
+
+function getValueFromRow(row, compoundFieldName) {
+    const [tableName, fieldName] = compoundFieldName.split('.');
+    return row[`${tableName}.${fieldName}`] || row[fieldName];
+}
+
+function performRightJoin(data, joinData, joinCondition, fields, table) {
+    // Cache the structure of a main table row (keys only)
+    const mainTableRowStructure = data.length > 0 ? Object.keys(data[0]).reduce((acc, key) => {
+        acc[key] = null; // Set all values to null initially
+        return acc;
+    }, {}) : {};
+
+    return joinData.map(joinRow => {
+        const mainRowMatch = data.find(mainRow => {
+            const mainValue = getValueFromRow(mainRow, joinCondition.left);
+            const joinValue = getValueFromRow(joinRow, joinCondition.right);
+            return mainValue === joinValue;
+        });
+
+        // Use the cached structure if no match is found
+        const mainRowToUse = mainRowMatch || mainTableRowStructure;
+
+        // Include all necessary fields from the 'student' table
+        return createResultRow(mainRowToUse, joinRow, fields, table, true);
+    });
+}
+
+function createResultRow(mainRow, joinRow, fields, table, includeAllMainFields) {
+    const resultRow = {};
+
+    if (includeAllMainFields) {
+        // Include all fields from the main table
+        Object.keys(mainRow || {}).forEach(key => {
+            const prefixedKey = `${table}.${key}`;
+            resultRow[prefixedKey] = mainRow ? mainRow[key] : null;
+        });
+    }
+
+    // Now, add or overwrite with the fields specified in the query
+    fields.forEach(field => {
+        const [tableName, fieldName] = field.includes('.') ? field.split('.') : [table, field];
+        resultRow[field] = tableName === table && mainRow ? mainRow[fieldName] : joinRow ? joinRow[fieldName] : null;
+    });
+
+    return resultRow;
+}
+
+async function executeSELECTQuery(query) {
+    const { fields, table, whereClauses, joinType, joinTable, joinCondition } = parseQuery(query);
+    let data = await readCSV(`${table}.csv`);
+
+    // Perform INNER JOIN if specified
+    if (joinTable && joinCondition) {
+        const joinData = await readCSV(`${joinTable}.csv`);
+        switch (joinType.toUpperCase()) {
+            case 'INNER':
+                data = performInnerJoin(data, joinData, joinCondition, fields, table);
+                break;
+            case 'LEFT':
+                data = performLeftJoin(data, joinData, joinCondition, fields, table);
+                break;
+            case 'RIGHT':
+                data = performRightJoin(data, joinData, joinCondition, fields, table);
+                break;
+            default:
+                throw new Error(`Unsupported JOIN type: ${joinType}`);
+        }
+    }
+
+    console.log("AFTER JOIN", data);
+    console.log("WHERE CLAUSES", whereClauses);
+    // Apply WHERE clause filtering after JOIN (or on the original data if no join)
+    const filteredData = whereClauses.length > 0
+        ? data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
+        : data;
+
+    // console.log("AFTER WHERE", filteredData);
 
     // Select the specified fields
     return filteredData.map(row => {
@@ -247,15 +125,50 @@ const filteredData = whereClauses.length > 0
 }
 
 function evaluateCondition(row, clause) {
-    const { field, operator, value } = clause;
+    let { field, operator, value } = clause;
+
+    // Check if the field exists in the row
+    if (row[field] === undefined) {
+        throw new Error(`Invalid field: ${field}`);
+    }
+
+    // Parse row value and condition value based on their actual types
+    const rowValue = parseValue(row[field]);
+    let conditionValue = parseValue(value);
+
+    // console.log("EVALUATING", rowValue, operator, conditionValue, typeof (rowValue), typeof (conditionValue));
+
     switch (operator) {
-        case '=': return row[field] === value;
-        case '!=': return row[field] !== value;
-        case '>': return row[field] > value;
-        case '<': return row[field] < value;
-        case '>=': return row[field] >= value;
-        case '<=': return row[field] <= value;
+        case '=': return rowValue === conditionValue;
+        case '!=': return rowValue !== conditionValue;
+        case '>': return rowValue > conditionValue;
+        case '<': return rowValue < conditionValue;
+        case '>=': return rowValue >= conditionValue;
+        case '<=': return rowValue <= conditionValue;
         default: throw new Error(`Unsupported operator: ${operator}`);
     }
 }
+
+// Helper function to parse value based on its apparent type
+function parseValue(value) {
+
+    // Return null or undefined as is
+    if (value === null || value === undefined) {
+        return value;
+    }
+
+    // If the value is a string enclosed in single or double quotes, remove them
+    if (typeof value === 'string' && ((value.startsWith("'") && value.endsWith("'")) || (value.startsWith('"') && value.endsWith('"')))) {
+        value = value.substring(1, value.length - 1);
+    }
+
+    // Check if value is a number
+    if (!isNaN(value) && value.trim() !== '') {
+        return Number(value);
+    }
+    // Assume value is a string if not a number
+    return value;
+}
+
+
 module.exports = executeSELECTQuery;
