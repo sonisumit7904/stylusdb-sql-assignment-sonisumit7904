4ac907268156b54c09d0edc38f84e5627442fcc0
Sumit Soni <122284205+sonisumit7904@users.noreply.github.com>
2024-04-04 14:40:32 +0530

    pass test5

--- tests/step-04/index.test.js
+++ tests/step-04/index.test.js
@@ -1,7 +1,6 @@
 const readCSV = require('../../src/csvReader');
 const parseQuery = require('../../src/queryParser');
 const executeSELECTQuery = require('../../src/index');
-// const executeSELECTQuery = require('../src/index');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./student.csv');
@@ -22,19 +21,6 @@ test('Parse SQL Query', () => {
         joinTable: null
     });
 });
-
-test('Execute SQL Query', async () => {
-    const query = 'SELECT id, name FROM student';
-    const result = await executeSELECTQuery(query);
-    expect(result.length).toBeGreaterThan(0);
-    expect(result[0]).toHaveProperty('id');
-    expect(result[0]).toHaveProperty('name');
-    expect(result[0]).not.toHaveProperty('age');
-    expect(result[0]).toEqual({ id: '1', name: 'John' });
-});
-
-// tests/index.test.js
-
 
 test('Execute SQL Query', async () => {
     const query = 'SELECT id, name FROM student';
