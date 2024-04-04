a6d0d41f123c38556b2df3f7fb0b71a9b28093c0
Sumit Soni <122284205+sonisumit7904@users.noreply.github.com>
2024-04-04 16:17:34 +0530

    passed upto test 8

--- src/queryParser.js
+++ src/queryParser.js
@@ -1,289 +1,3 @@
-// // src/queryParser.js
-// // 8th step
-// // function parseQuery(query) {
-// //     const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
-// //     const match = query.match(selectRegex);
-
-// //     if (match) {
-// //         const [, fields, table, whereString] = match;
-// //         const whereClauses = whereString ? parseWhereClause(whereString) : [];
-// //         return {
-// //             fields: fields.split(',').map(field => field.trim()),
-// //             table: table.trim(),
-// //             whereClauses
-// //         };
-// //     } else {
-// //         throw new Error('Invalid query format');
-// //     }
-// // }
-
-// // src/queryParser.js
-
-// function parseQuery(query) {
-//     // First, let's trim the query to remove any leading/trailing whitespaces
-//     query = query.trim();
-
-//     // Initialize variables for different parts of the query
-//     let selectPart, fromPart;
-
-//     // Split the query at the WHERE clause if it exists
-//     const whereSplit = query.split(/\sWHERE\s/i);
-//     query = whereSplit[0]; // Everything before WHERE clause
-
-//     // WHERE clause is the second part after splitting, if it exists
-//     const whereClause = whereSplit.length > 1 ? whereSplit[1].trim() : null;
-
-//     // Split the remaining query at the JOIN clause if it exists
-//     const joinSplit = query.split(/\sINNER JOIN\s/i);
-//     selectPart = joinSplit[0].trim(); // Everything before JOIN clause
-
-//     // JOIN clause is the second part after splitting, if it exists
-//     const joinPart = joinSplit.length > 1 ? joinSplit[1].trim() : null;
-
-//     // Parse the SELECT part
-//     const selectRegex = /^SELECT\s(.+?)\sFROM\s(.+)/i;
-//     const selectMatch = selectPart.match(selectRegex);
-//     if (!selectMatch) {
-//         throw new Error('Invalid SELECT format');
-//     }
-
-//     const [, fields, table] = selectMatch;
-
-//     // Parse the JOIN part if it exists
-//     let joinTable = null, joinCondition = null;
-//     if (joinPart) {
-//         const joinRegex = /^(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
-//         const joinMatch = joinPart.match(joinRegex);
-//         if (!joinMatch) {
-//             throw new Error('Invalid JOIN format');
-//         }
-
-//         joinTable = joinMatch[1].trim();
-//         joinCondition = {
-//             left: joinMatch[2].trim(),
-//             right: joinMatch[3].trim()
-//         };
-//     }
-
-//     // Parse the WHERE part if it exists
-//     let whereClauses = [];
-//     if (whereClause) {
-//         whereClauses = parseWhereClause(whereClause);
-//     }
-
-//     return {
-//         fields: fields.split(',').map(field => field.trim()),
-//         table: table.trim(),
-//         whereClauses,
-//         joinTable,
-//         joinCondition
-//     };
-// }
-
-// //6TH STEP
-// // function parseWhereClause(whereString) {
-// //     const conditions = whereString.split(/ AND | OR /i);
-// //     return conditions.map(condition => {
-// //         const [field, operator, value] = condition.split(/\s+/);
-// //         return { field, operator, value };
-// //     });
-// // }
-
-// // src/queryParser.js
-// function parseWhereClause(whereString) {
-//     const conditionRegex = /(.*?)(=|!=|>|<|>=|<=)(.*)/;
-//     return whereString.split(/ AND | OR /i).map(conditionString => {
-//         const match = conditionString.match(conditionRegex);
-//         if (match) {
-//             const [, field, operator, value] = match;
-//             return { field: field.trim(), operator, value: value.trim() };
-//         }
-//         throw new Error('Invalid WHERE clause format');
-//     });
-// }
-
-// module.exports = parseQuery;
-
-//step1234
-// function parseQuery(query) {
-//     const selectRegex = /SELECT (.+) FROM (.+)/i;
-//     const match = query.match(selectRegex);
-
-//     if (match) {
-//         const [, fields, table] = match;
-//         return {
-//             fields: fields.split(',').map(field => field.trim()),
-//             table: table.trim()
-//         };
-//     } else {
-//         throw new Error('Invalid query format');
-//     }
-// }
-
-// module.exports = parseQuery;
-
-
-//step5
-// src/queryParser.js
-
-// function parseQuery(query) {
-//     const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
-//     const match = query.match(selectRegex);
-
-//     if (match) {
-//         const [, fields, table, whereClause] = match;
-//         return {
-//             fields: fields.split(',').map(field => field.trim()),
-//             table: table.trim(),
-//             whereClause: whereClause ? whereClause.trim() : null
-//         };
-//     } else {
-//         throw new Error('Invalid query format');
-//     }
-// }
-
-// module.exports = parseQuery;
-
-
-// step6
-// src/queryParser.js
-
-// function parseQuery(query) {
-//     const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
-//     const match = query.match(selectRegex);
-
-//     if (match) {
-//         const [, fields, table, whereString] = match;
-//         const whereClauses = whereString ? parseWhereClause(whereString) : [];
-//         return {
-//             fields: fields.split(',').map(field => field.trim()),
-//             table: table.trim(),
-//             whereClauses
-//         };
-//     } else {
-//         throw new Error('Invalid query format');
-//     }
-// }
-
-// function parseWhereClause(whereString) {
-//     const conditions = whereString.split(/ AND | OR /i);
-//     return conditions.map(condition => {
-//         const [field, operator, value] = condition.split(/\s+/);
-//         return { field, operator, value };
-//     });
-// }
-
-// module.exports = parseQuery;
-
-
-//step7
-// src/queryParser.js
-// function parseQuery(query) {
-//     const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
-//     const match = query.match(selectRegex);
-
-//     if (match) {
-//         const [, fields, table, whereString] = match;
-//         const whereClauses = whereString ? parseWhereClause(whereString) : [];
-//         return {
-//             fields: fields.split(',').map(field => field.trim()),
-//             table: table.trim(),
-//             whereClauses
-//         };
-//     } else {
-//         throw new Error('Invalid query format');
-//     }
-// }
-
-// function parseWhereClause(whereString) {
-//     const conditionRegex = /(.*?)(=|!=|>|<|>=|<=)(.*)/;
-//     return whereString.split(/ AND | OR /i).map(conditionString => {
-//         const match = conditionString.match(conditionRegex);
-//         if (match) {
-//             const [, field, operator, value] = match;
-//             return { field: field.trim(), operator, value: value.trim() };
-//         }
-//         throw new Error('Invalid WHERE clause format');
-//     });
-// }
-// module.exports = parseQuery;
-
-//STEP8
-// function parseQuery(query) {
-//     // First, let's trim the query to remove any leading/trailing whitespaces
-//     query = query.trim();
-
-//     // Initialize variables for different parts of the query
-//     let selectPart, fromPart;
-
-//     // Split the query at the WHERE clause if it exists
-//     const whereSplit = query.split(/\sWHERE\s/i);
-//     query = whereSplit[0]; // Everything before WHERE clause
-
-//     // WHERE clause is the second part after splitting, if it exists
-//     const whereClause = whereSplit.length > 1 ? whereSplit[1].trim() : null;
-
-//     // Split the remaining query at the JOIN clause if it exists
-//     const joinSplit = query.split(/\sINNER JOIN\s/i);
-//     selectPart = joinSplit[0].trim(); // Everything before JOIN clause
-
-//     // JOIN clause is the second part after splitting, if it exists
-//     const joinPart = joinSplit.length > 1 ? joinSplit[1].trim() : null;
-
-//     // Parse the SELECT part
-//     const selectRegex = /^SELECT\s(.+?)\sFROM\s(.+)/i;
-//     const selectMatch = selectPart.match(selectRegex);
-//     if (!selectMatch) {
-//         throw new Error('Invalid SELECT format');
-//     }
-
-//     const [, fields, table] = selectMatch;
-
-//     // Parse the JOIN part if it exists
-//     let joinTable = null, joinCondition = null;
-//     if (joinPart) {
-//         const joinRegex = /^(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
-//         const joinMatch = joinPart.match(joinRegex);
-//         if (!joinMatch) {
-//             throw new Error('Invalid JOIN format');
-//         }
-
-//         joinTable = joinMatch[1].trim();
-//         joinCondition = {
-//             left: joinMatch[2].trim(),
-//             right: joinMatch[3].trim()
-//         };
-//     }
-
-//     // Parse the WHERE part if it exists
-//     let whereClauses = [];
-//     if (whereClause) {
-//         whereClauses = parseWhereClause(whereClause);
-//     }
-
-//     return {
-//         fields: fields.split(',').map(field => field.trim()),
-//         table: table.trim(),
-//         whereClauses,
-//         joinTable,
-//         joinCondition
-//     };
-// }
-
-// function parseWhereClause(whereString) {
-//     const conditionRegex = /(.*?)(=|!=|>|<|>=|<=)(.*)/;
-//     return whereString.split(/ AND | OR /i).map(conditionString => {
-//         const match = conditionString.match(conditionRegex);
-//         if (match) {
-//             const [, field, operator, value] = match;
-//             return { field: field.trim(), operator, value: value.trim() };
-//         }
-//         throw new Error('Invalid WHERE clause format');
-//     });
-// }
-// module.exports = parseQuery;
-
-//step9
 /*
 Creating a Query Parser which can parse SQL `SELECT` Queries only.
 */
@@ -302,8 +16,11 @@ function parseQuery(query) {
     const whereClause = whereSplit.length > 1 ? whereSplit[1].trim() : null;
 
     // Split the remaining query at the JOIN clause if it exists
-    const joinSplit = query.split(/\s(INNER|LEFT|RIGHT) JOIN\s/i);
+    const joinSplit = query.split(/\sINNER JOIN\s/i);
     selectPart = joinSplit[0].trim(); // Everything before JOIN clause
+
+    // JOIN clause is the second part after splitting, if it exists
+    const joinPart = joinSplit.length > 1 ? joinSplit[1].trim() : null;
 
     // Parse the SELECT part
     const selectRegex = /^SELECT\s(.+?)\sFROM\s(.+)/i;
@@ -314,7 +31,21 @@ function parseQuery(query) {
 
     const [, fields, table] = selectMatch;
 
-    const { joinType, joinTable, joinCondition } = parseJoinClause(query);
+    // Parse the JOIN part if it exists
+    let joinTable = null, joinCondition = null;
+    if (joinPart) {
+        const joinRegex = /^(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
+        const joinMatch = joinPart.match(joinRegex);
+        if (!joinMatch) {
+            throw new Error('Invalid JOIN format');
+        }
+
+        joinTable = joinMatch[1].trim();
+        joinCondition = {
+            left: joinMatch[2].trim(),
+            right: joinMatch[3].trim()
+        };
+    }
 
     // Parse the WHERE part if it exists
     let whereClauses = [];
@@ -326,7 +57,6 @@ function parseQuery(query) {
         fields: fields.split(',').map(field => field.trim()),
         table: table.trim(),
         whereClauses,
-        joinType,
         joinTable,
         joinCondition
     };
@@ -344,26 +74,4 @@ function parseWhereClause(whereString) {
     });
 }
 
-function parseJoinClause(query) {
-    const joinRegex = /\s(INNER|LEFT|RIGHT) JOIN\s(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
-    const joinMatch = query.match(joinRegex);
-
-    if (joinMatch) {
-        return {
-            joinType: joinMatch[1].trim(),
-            joinTable: joinMatch[2].trim(),
-            joinCondition: {
-                left: joinMatch[3].trim(),
-                right: joinMatch[4].trim()
-            }
-        };
-    }
-
-    return {
-        joinType: null,
-        joinTable: null,
-        joinCondition: null
-    };
-}
-
-module.exports = { parseQuery, parseJoinClause };
+module.exports = parseQuery;
